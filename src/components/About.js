1539a6c77f164fd41156abfa81d92b91e3ef94b6
andrewroddy92 <andrewroddy92@yahoo.com>
2021-11-24 18:58:34 -0500

    complete styling home and about

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,37 @@
 import React from "react";
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+
 export default function About() {
+    const styles = {
+      container: {
+        paddingTop: '1.5rem'
+      },
+      header: {
+        color: '#E63946',
+        fontSize: '1.5rem'
+      },
+      text: {
+        color: "#F1FAEE",
+        fontSize: '1.5rem'
+      }
+    };
+
     return (
-        <section id="about">
-          <h1>about me</h1>
-          <p>Hello -- my name is Andrew and I <br/> develop, test, and deploy web applications.</p>
-          <p><br/>After graduating with a B.S. in Technical Communication, <br/> I spent a few years professionally communicating <br/> product requirements between business & IT.</p>
-          <p><br/>In a change of career, I've retooled my skillset to <br/> bring those requirements to life.</p>
-        </section>
+      <Container id="about" style={styles.container}>
+        <Row>
+          <Col>
+            <h1 style={styles.header}>about me</h1>
+            <section style={styles.text}>
+              <p>Hello -- my name is Andrew and I <br/> develop, test, and deploy web applications.</p>
+              <p><br/>After graduating with a B.S. in Technical Communication, I spent a few years professionally communicating product requirements between business & IT.</p>
+              <p><br/>In a change of career, I've retooled my skillset to bring those requirements to life.</p>
+            </section>
+          </Col>
+        </Row>
+
+      </Container>
       );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,31 +5,39 @@ import Col from 'react-bootstrap/Col'
 
 export default function Home() {
     const styles = {
+      container: {
+        paddingTop: '1.5rem'
+      },
       intro: {
         color: '#A8DADC',
+        fontSize: '1.5rem'
       },
       name: {
         color: '#E63946',
+        fontSize: '3.5rem'
       },
       text: {
-        color: "#F1FAEE"
+        color: "#F1FAEE",
+        fontSize: '1.5rem'
       }
     };
 
     return (
-      <Container id="home">
+      <Container id="home" style={styles.container}>
           <Row>
             <Col>
-              <h3 style={styles.intro}>my name is</h3>
-              <h1 style={styles.name}>Andrew Roddy.</h1>
-              <p style={styles.text}>I'm a full stack web developer<br/>
-              based in the Greater Atlanta Area<br/>
-              seeking the opportunity to build<br/>
-              high quality, high value applications.</p>
-              <br/>
-              <p>in: <a href = ">https://www.linkedin.com/in/andrew-roddy/">https://www.linkedin.com/in/andrew-roddy/</a></p>
-              <p>git: <a href = "https://github.com/andrewroddy92">https://github.com/andrewroddy92</a></p>
-              <p>email: <a href = "mailto: andrewroddy92@yahoo.com">andrewroddy92@yahoo.com</a></p>
+              <h1 style={styles.intro}>my name is</h1>
+              <h2 style={styles.name}>Andrew Roddy.</h2>
+              <section style={styles.text}>
+                <p>I'm a full stack web developer<br/>
+                based in the Greater Atlanta Area<br/>
+                seeking the opportunity to build<br/>
+                high quality, high value wep apps.</p>
+                <br/>
+                <p>in: <a href = ">https://www.linkedin.com/in/andrew-roddy/">https://www.linkedin.com/in/andrew-roddy/</a></p>
+                <p>git: <a href = "https://github.com/andrewroddy92">https://github.com/andrewroddy92</a></p>
+                <p>email: <a href = "mailto: andrewroddy92@yahoo.com">andrewroddy92@yahoo.com</a></p>
+              </section>
             </Col>
           </Row>
       </Container>
