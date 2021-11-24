04816b9b65d7dba9cf54e1c44ca2c62d8995182b
andrewroddy92 <andrewroddy92@yahoo.com>
2021-11-24 09:09:13 -0500

    adding navbar and styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,16 @@
 import React from "react";
 export default function Navbar() {
+
     return(
-        <section id="nav"/>
+        <header>
+            <h1>Andrew Roddy</h1>
+            <nav>
+                <ul>
+                    <li><a href="#about-me">About Me</a></li>
+                    <li><a href="#works">My Works</a></li>
+                    <li><a href="#contact">Contact Me</a></li>
+                </ul>
+            </nav>
+        </header>
     );
 }
